2075eb8ea57c450b2334fd568bfcca507612a3a3
Nurlan Bolayev <bolayev.nurlan@gmail.com>
2021-01-20 21:03:40 +0400

    Implement PostField component

---
+++ src/components/PostField.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <ReplyField
+        :value="value"
+        :loading="loading"
+        @input="$emit('input', $event)"
+        placeholder="What's in your mind?"
+        allow-enter
+    />
+
+    <v-btn
+        :loading="loading"
+        class="post-button"
+        icon
+        color="blue darken-1"
+        @click="value.length && $emit('submit', value)"
+    >
+      <v-icon>
+        mdi-arrow-right-circle
+      </v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+import ReplyField from "@/components/ReplyField";
+
+export default {
+  components: {ReplyField},
+  props: {
+    value: {
+      required: true,
+      default: '',
+    },
+
+    loading: {
+      type: Boolean,
+      default: false,
+    }
+  },
+}
+</script>
+
+<style>
+.container {
+  position: relative;
+}
+
+.post-button {
+  position: absolute;
+  right: 18px;
+  bottom: 14px;
+}
+</style>
